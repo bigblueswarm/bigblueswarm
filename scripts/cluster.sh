82085a331950e8bd93e6ab58f833217072380eb8
LEDUNOIS Simon <simon.ledunois@gmail.com>
2021-10-25 15:07:28 +0000

    chore() add script project management

---
+++ scripts/cluster.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+start() {
+    docker-compose -f "$(dirname "$0")/docker-compose.yml" up -d
+}
+
+stop() {
+    docker rm -f bbb1 bbb2 influxdb
+}
+
+for param in "$@"
+do
+  case $param in
+    start)
+      start
+      ;;
+    stop)
+      stop
+      ;;
+    *)
+      echo "Invalid argument : $param"
+  esac
+  if [ ! $? -eq 0 ]; then
+    exit 1
+  fi
+done
