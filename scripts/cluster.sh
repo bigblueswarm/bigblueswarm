700f2610130ddab203bf39010539d9665bbbcb89
LEDUNOIS Simon <simon.ledunois@gmail.com>
2021-10-26 09:02:40 +0000

    chore() Add usage and set influxdb token option

--- scripts/cluster.sh
+++ scripts/cluster.sh
@@ -1,28 +1,50 @@
 #!/bin/bash
 
 start() {
+    echo "Starting BigBlueButton cluster..."
     docker-compose -f "$(dirname "$0")/docker-compose.yml" up -d
 }
 
 stop() {
+    echo "Stopping cluster..."
     docker rm -f bbb1 bbb2 influxdb
 }
 
 usage() {
-  echo "Help!"
+  echo "
+    Usage: cluster.sh [OPTION]
+
+    Manage local development cluster.
+    -r, --run, --start        Start development cluster. It starts an InfluxDB serveur and two BigBlueButton servers.
+    -s, --stop                Stop development cluster.
+    -t, --set-token [token]   Update BigBlueButton server to use given InfluxDB token.
+    -h, --help                Display this help and exit
+  "
+}
+
+set_influxdb_token() {
+  echo "Setting up InfluxDB token..."
+  docker exec -it bbb1 sh -c "echo INFLUXDB_TOKEN=$1 > /etc/default/telegraf && . /etc/default/telegraf && systemctl restart telegraf"
+  docker exec -it bbb2 sh -c "echo INFLUXDB_TOKEN=$1 > /etc/default/telegraf && . /etc/default/telegraf && systemctl restart telegraf"
+  echo "Done"
 }
 
 for param in "$@"
 do
   case $param in
-    -run | --start)
+    -r | --run | --start)
       start
       ;;
-    --stop)
+    -s | --stop)
       stop
+      ;;
+    -t | --set-token)
+      set_influxdb_token "$2"
+      exit 1
       ;;
     -h | --help)
       usage
+      exit 1
       ;;
     *)
       echo "Invalid argument : $param"
