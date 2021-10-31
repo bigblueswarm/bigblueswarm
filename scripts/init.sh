a43499d45cb33281a3fb8abd45610e30ee16a790
LEDUNOIS Simon <simon.ledunois@gmail.com>
2021-10-31 11:31:48 +0000

    ci() Add pre-commit lint and github action golangc-lint

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 go get -u github.com/gin-gonic/gin
+
+# pre-commit installation
+curl https://pre-commit.com/install-local.py | python -
+source ~/.profile
+
+pre-commit --version
+pre-commit install
