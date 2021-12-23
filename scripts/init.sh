fb1a96070e2440ce372040e1c4aab291126954dd
LEDUNOIS Simon <simon.ledunois@gmail.com>
2021-12-23 15:23:52 +0000

    ci: add conventional pre-commit hook #20

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,5 +8,11 @@ go get -u github.com/gin-gonic/gin
 curl https://pre-commit.com/install-local.py | python -
 source ~/.profile
 
+pip install pre-commit
+
 pre-commit --version
 pre-commit install
+
+npm install --save-dev @commitlint/{config-conventional,cli} husky
+npx husky install
+npx husky add .husky/commit-msg "npx --no -- commitlint --edit \"$1\""
