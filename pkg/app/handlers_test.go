043685152f24f17072f270e4d113b04d8093b84b
LEDUNOIS Simon <simon.ledunois@gmail.com>
2021-12-24 14:17:57 +0000

    fix: move TestMain function into its own `main_test.go` file #24

--- pkg/app/handlers_test.go
+++ pkg/app/handlers_test.go
@@ -2,12 +2,22 @@ package app
 
 import (
 	"b3lb/pkg/api"
+	"b3lb/pkg/config"
 	"encoding/xml"
 	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestHealthCheckRoute(t *testing.T) {
+	router := launchRouter(&config.Config{})
+	w := executeRequest(router, "GET", "/bigbluebutton/api", nil)
+
+	response := "<response><returncode>SUCCESS</returncode><version>2.0</version></response>"
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, response, w.Body.String())
+}
 
 func TestCreate(t *testing.T) {
 	type test struct {
