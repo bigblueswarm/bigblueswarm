79417e7043f1295522356ae5eb7624feac0d68b8
LEDUNOIS Simon <simon.ledunois@gmail.com>
2022-01-20 18:00:34 +0000

    chore: prefix meetings in redis by `meeting:` #62

--- pkg/app/session_manager.go
+++ pkg/app/session_manager.go
@@ -27,23 +27,28 @@ func NewSessionManager(rdb redis.Client) SessionManager {
 	}
 }
 
+// cacheKey format meetingID as a valid meeting cache key
+func cacheKey(id string) string {
+	return "meeting:" + id
+}
+
 // Add persist the session in the redis database
 func (m *RedisSessionManager) Add(sessionID string, host string) error {
-	_, err := m.RDB.Set(context.Background(), sessionID, host, 0).Result()
+	_, err := m.RDB.Set(context.Background(), cacheKey(sessionID), host, 0).Result()
 
 	return utils.ComputeErr(err)
 }
 
 // Get retrieve the session from the redis database
 func (m *RedisSessionManager) Get(sessionID string) (string, error) {
-	host, err := m.RDB.Get(context.Background(), sessionID).Result()
+	host, err := m.RDB.Get(context.Background(), cacheKey(sessionID)).Result()
 
 	return host, utils.ComputeErr(err)
 }
 
 // Remove remove the session from the redis database
 func (m *RedisSessionManager) Remove(sessionID string) error {
-	_, err := m.RDB.Del(context.Background(), sessionID).Result()
+	_, err := m.RDB.Del(context.Background(), cacheKey(sessionID)).Result()
 
 	return utils.ComputeErr(err)
 }
