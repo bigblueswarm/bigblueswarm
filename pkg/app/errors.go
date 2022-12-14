e9605a6e607f59252f9da376a345f4c0516d9a10
LEDUNOIS Simon <simon.ledunois@dental-wings.com>
2022-12-14 09:43:57 -0500

    feat: create custom errors #164

---
+++ pkg/app/errors.go
@@ -0,0 +1,24 @@
+// Package app is the bigblueswarm core
+package app
+
+import "github.com/bigblueswarm/bigblueswarm/v2/pkg/api"
+
+func noInstanceFoundError() *api.Error {
+	return api.CreateError("noInstanceFound", "BigBlueSwarm do not found a valid BigBlueButton instance for your request")
+}
+
+func serverError(message string) *api.Error {
+	return api.CreateError("internalError", message)
+}
+
+func getTenantError() *api.Error {
+	return serverError("BigBlueSwarm failed to retrieve the request tenant")
+}
+
+func meetingPoolReachedError() *api.Error {
+	return api.CreateError("meetingPoolReached", "Your tenant reached the meeting pool limit and can't create a new one.")
+}
+
+func userPoolReachedError() *api.Error {
+	return api.CreateError("userPoolReached", "Your tenant reached the user pool limit.")
+}
